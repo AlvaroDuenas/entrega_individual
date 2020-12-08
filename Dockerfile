9ae52af077cb020d5213412a096eebf5268fc99e
AlvaroDuenas <alvaro25duenas@gmail.com>
2020-12-08 18:34:34 +0000

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
+# Carga la imagen de docker correspondiente a alpine con python
 FROM python:3.7-alpine
+# Establece el entorno de trabajo
 WORKDIR /code
+# Guarda las variables de entorno correspondientes a flask
+## La direccion del fichero a ejecutar
 ENV FLASK_APP=main.py
+## La direccion donde se almacenara el servidor
 ENV FLASK_RUN_HOST=0.0.0.0
+# Se instalan los paquetes necesarios
 RUN apk add --no-cache gcc musl-dev linux-headers
+# Se copian e instalan las librerias de python necesarias para ejecutar la aplicacion de flask
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+# Se exponen los puertos de flask y de mango-express
 EXPOSE 5000 8081
+# Se copian los ficheros en el contenedor
 COPY . .
+# Se ejecuta la aplicacion flask
 CMD ["flask", "run"]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
-from urllib.parse import quote_plus
+# Carga de la aplicacion flask
 app = Flask(__name__)
 """
 user = os.environ['MONGODB_USERNAME']
@@ -14,23 +14,29 @@ client = MongoClient("mongo:27017",
 	authSource=db_name,
 	authMechanism='SCRAM-SHA-1')
 """
+# Carga del cliente Mongo
 client = MongoClient("mongo", 27017)
+# Acceso a la base de datos
 db = client.appdb
 
+# Pagina principal de la aplicacion
+## Corresponde al textlabel y boton para almacenar un nuevo objeto.
 @app.route("/")
 def index():
+## Obtiene las colecciones existentes
     _items = db.appdb.find()
     items = [items for items in _items]
-
+## Muestra el fichero html en el servidor
     return render_template("index.html", items=items)
 
-
+# Anade la nueva entrada a la base de datos y se redirige a la pagina inicial
 @app.route("/new", methods=["POST"])
 def new():
+## Obtiene el nuevo objeto en formato json
     data = {
         "helloworld": request.form["helloworld"]
     }
-
+## Inserta el objeto en la base de datos
     db.appdb.insert_one(data)
-
+## Redirige la interfaz a la pagina inicial
     return redirect(url_for("index"))
