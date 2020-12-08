17b0bb345dc176ceb4556402c296c058772c8b90
AlvaroDuenas <alvaro25duenas@gmail.com>
2020-12-08 15:17:39 +0000

    Update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 app = Flask(__name__)
-
+"""
 user = os.environ['MONGODB_USERNAME']
 password = os.environ['MONGODB_PASSWORD']
 db_name = os.environ['MONGODB_DATABASE']
@@ -13,8 +13,8 @@ client = MongoClient("mongo:27017",
 	password=password,
 	authSource=db_name,
 	authMechanism='SCRAM-SHA-1')
-
-#client = MongoClient("mongodb", 27018)
+"""
+client = MongoClient("mongo", 27017)
 db = client.appdb
 
 @app.route("/")
